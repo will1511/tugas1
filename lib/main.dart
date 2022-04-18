ee4bc1aa10e45f30f01ab828591fd465a5eaf8c6
will1511 <willy.535200035@stu.untar.ac.id>
2022-04-18 23:22:08 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo'),
-          backgroundColor: Colors.teal,
-        ),
-        body: Center(
-          child: Image(
-              image: NetworkImage(
-                  'https://cdn.pixabay.com/photo/2022/04/04/02/52/cherry-blossoms-7110279_960_720.jpg')),
-        ),
-      ),
-    );
+        title: 'Flutter Demo',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Flutter Demo'),
+              backgroundColor: Colors.teal,
+            ),
+            body: Center(
+              child: Column(
+                children: [
+                  Image(
+                    image: AssetImage('images/logo.png'),
+                  ),
+                  Text('Hello World!'),
+                ],
+              ),
+            )));
   }
 }
